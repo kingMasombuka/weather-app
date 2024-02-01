5c962509438595866b6a7752b2a0fb44b4da7026
Senzo Ncongwane <senzoncongwane21@gmail.com>
2024-02-01 19:06:53 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+//onclick handler for button
+const APIKEY = '7c91e9214b87443394a190616233009';
+const citytosearch = document.getElementById('city');
+const degrees = document.querySelector('.city-details h3');
+const city = document.querySelector('.city-details h4');
+const weatherImg = document.querySelector('.city-details img');
+const humidity = document.getElementById('humidity');
+const windSpeed = document.getElementById('windSpeed');
+const imgElement = document.querySelector('.city-details img');
+
+async function getWeather() {
+    const cityToFind = citytosearch.value || "Manzini"
+	const response = await fetch(
+		`http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${cityToFind}`
+	);
+    const weatherData = await response.json()
+    console.log(weatherData)
+    degrees.innerHTML=parseInt(weatherData.current.feelslike_c)+"°C"
+    humidity.innerHTML=weatherData.current.humidity+"%"
+    windSpeed.innerHTML=weatherData.current.wind_kph+"km/h"
+    city.innerHTML=weatherData.location.name
+} 
+getWeather()
+function updateClock() {
+    const clockElement = document.getElementById('clock');
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    clockElement.textContent = timeString;
+  }
+  
+  // Update the clock every second
+  setInterval(updateClock, 1000);
+  
